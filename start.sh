30a36709932f6b2d3d1eb369df464e5674e27111
won <won@nhnedu.com>
2023-03-14 00:09:33 +0900

    Docker Log

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ python ./manage.py migrate
 echo '-------------------------------'
 echo 'Run server'
 echo '-------------------------------'
-python ./manage.py runserver 0.0.0.0:8000
+python -u ./manage.py runserver 0.0.0.0:8000
